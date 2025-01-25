7a69fd4a90e3c54e2b1e5dc1a65b8a5cbdf7de75
Mohammadreza Bonyadi <bonyadi.n2000@gmail.com>
2025-01-25 07:46:06 +0330

    Refactor registration to use RegisterDto for improved data handling

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,13 +11,13 @@ namespace API.Controllers;
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost]
-    public async Task<ActionResult<AppUsers>> Register(string username, string password)
+    public async Task<ActionResult<AppUsers>> Register(RegisterDto registerDto)
     {
         using var hmc = new HMACSHA512();
         AppUsers user = new AppUsers
         {
-            UserName = username,
-            PasswordHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username,
+            PasswordHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmc.Key
         };
         context.Users.Add(user);
