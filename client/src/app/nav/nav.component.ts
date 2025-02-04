cd23bd16e48aec9f52202bba177374411b380089
Mohammadreza Bonyadi <bonyadi.n2000@gmail.com>
2025-02-04 12:27:01 +0330

    Enhance navigation component: add conditional rendering for user login state, implement logout functionality, and update favicon to SVG format

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -7,20 +7,25 @@ import { AccountService } from '../_services/account.service';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
-  private accountServices = inject(AccountService);
+  private readonly accountServices = inject(AccountService);
   loggedIn = false;
   model: any = {};
 
-  login(){
-    this.accountServices.login(this.model).subscribe(response => {
-      console.log(response);
-      this.loggedIn = true;
-    }, error => {
-      console.log(error);
-    });
+  login() {
+    this.accountServices.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
+  logout() {
+    this.loggedIn = false;
+  }
 }
