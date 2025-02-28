a2c4acfcc577d539754f540240b90c10987af1f6
MohammadrezaBonyadi <bonyadi.n2000@gmail.com>
2025-02-28 18:22:12 +0330

    Inject data to database

---
+++ API/Data/Seed.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class Seed
+{
+    public static async Task SeedUser(DataContext db)
+    {
+        if (await db.Users.AnyAsync())
+        {
+            return;
+        }
+        
+        string userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+        
+        JsonSerializerOptions options = 
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        
+        List<AppUsers>? users = 
+            JsonSerializer.Deserialize<List<AppUsers>>(userData, options);
+
+        if (users is null)
+        {
+            return;
+        }
+
+        foreach (AppUsers user in users)
+        {
+            using HMACSHA512 hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+            user.PasswordSalt = hmac.Key;
+            db.Users.Add(user);
+        }
+        await db.SaveChangesAsync();
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,5 +35,19 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 app.MapControllers();
+
+using IServiceScope scope = app.Services.CreateScope();
+IServiceProvider services = scope.ServiceProvider;
+try
+{
+    DataContext context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUser(context);
+}
+catch (Exception ex)
+{
+    ILogger logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex.Message, "An error occurred while migrating the database.");
+}
 
 await app.RunAsync();
